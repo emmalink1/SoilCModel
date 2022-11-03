b857adb19e2ac72de8152bede02e2e26d85f5ddf
emmalink1@gmaill.com <link0126@ln0005.mesabi.msi.umn.edu>
2022-11-02 21:16:07 -0500

    Code in MVP2.py working. Need to update README.txt

--- MVP2.py
+++ MVP2.py
@@ -10,62 +10,65 @@ import rasterio
 import rioxarray as rxr
 from rasterio.plot import plotting_extent
 
-print("here")
+import shapely
+import warnings
+from shapely.errors import ShapelyDeprecationWarning
+warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning) 
+pd.options.mode.chained_assignment = None
 
 ## Import soils data
-## National Cooperative soil survey point data for world
+### National Cooperative soil survey point data for world
 NCSS_file = "./MVP1_data/NCSS_Soil_Characterization_Database_09_14_2018.gdb"
-
-print("here1")
-## Subset dataframes to USA
 
 pedonSites = gpd.read_file(NCSS_file, layer='NCSS_Site_Location')
 
-# subsetting SSURGO data to site locations in MN
+## subsetting SSURGO data to site locations in MN
 pedonSites = gpd.read_file(NCSS_file, layer='NCSS_Site_Location')
 pedons_MN = pedonSites[(pedonSites['state_code']== "MN")]
 
-# When I map this, it is clear that there are some mislabeled pedons in canada so I take those out by latitude.
-# There is also one in ND but I'm not worried about that since it's close so I keep it in 
+### When I map this, it is clear that there are some mislabeled pedons in canada so I take those out by latitude.
+### There is also one in ND but I'm not worried about that since it's close so I keep it in 
 pedons_MN = pedons_MN[(pedons_MN['latitude_decimal_degrees'] < 60)]
 
-# also subsetting out to just the columns that I want/need since there are otherwise 68
+### also subsetting out to just the columns that I want/need since there are otherwise 68
 pedons_MN = pedons_MN[['site_key', 'site_obsdate', 'state_code', 'PedonName', 'geometry']]
 
-# Finally, add a column 'year' with observation year read from observation date string
+### Finally, add a column 'year' with observation year read from observation date string
 pedons_MN['year'] = pedons_MN['site_obsdate'].str[-4:]
 print(pedons_MN.head())
-print("here2")
 
 ## Wrangling and transofrmations of soil Pedon data
-## Unfortunately I have to do a 3-way join of the data :( 
-# Read in Pedon Taxonomy file
+### Unfortunately I have to do a 3-way join of the data :( 
+
+### Read in Pedon Taxonomy file
 NCSS_Pedon_Taxonomy = gpd.read_file(NCSS_file, layer='NCSS_Pedon_Taxonomy')
-# left joining to only keep MN subset but also keep all of it
-# I also only want a few columns out of the NCSS_Pedon_Taxonomy layer; pedon_key, site_key
+
+### left joining to only keep MN subset but also keep all of it
+### I also only want a few columns out of the NCSS_Pedon_Taxonomy layer; pedon_key, site_key
 Sites_attributes = pedons_MN.merge(NCSS_Pedon_Taxonomy[['pedon_key','site_key']], on = 'site_key', how = 'left')
 print(Sites_attributes.head())
-## Read in NCSS_Layer
+### Read in NCSS_Layer
 NCSS_Layer = gpd.read_file(NCSS_file, layer = 'NCSS_Layer')
 
 
-# Merge sites_attributes with NCSS_Layer
-# Again, I only want a subset of the columns, but this time I want more of them
+## Merge sites_attributes with NCSS_Layer
+#### Again, I only want a subset of the columns, but this time I want more of them
 Sites_attributes = Sites_attributes.merge(NCSS_Layer[['pedon_key','labsampnum','site_key','layer_sequence', 'layer_type','hzn_top', 'hzn_bot']], on = ['pedon_key','site_key'], how = 'left')
 
-# Read in Carbon_and_Extractions layer
+### Read in Carbon_and_Extractions layer
 Carbon_and_Extractions = gpd.read_file(NCSS_file, layer = 'Carbon_and_Extractions')
 #list(Carbon_and_Extractions.columns)
 
-# Merge sites_attributes with carbon_and_extractions layer
+### Merge sites_attributes with carbon_and_extractions layer
 Sites_attributes = Sites_attributes.merge(Carbon_and_Extractions[['labsampnum','c_tot','c_tot_code','oc','oc_code']], on = 'labsampnum', how = 'left')
 
-#Read in Bulk density layer
+###Read in Bulk density layer
 Bulk_Density_and_Moisture = gpd.read_file(NCSS_file, layer = 'Bulk_Density_and_Moisture')
-# Merge sites_attributes with bulk_density layer (selecting 'db_od', bulk density over dry, misspelled aghhhh)
+### Merge sites_attributes with bulk_density layer (selecting 'db_od', bulk density over dry, misspelled aghhhh)
+
 Sites_attributes = Sites_attributes.merge(Bulk_Density_and_Moisture[['labsampnum','db_od']], on = 'labsampnum', how = 'left')
 
-# Get rid of points that don't have bulk density and oc data as they're useless to us 
+### Get rid of points that don't have bulk density and oc data as they're useless to us 
 Sites_attributes = Sites_attributes[(Sites_attributes["db_od"].isnull() == False)]
 
 Sites_attributes = Sites_attributes[(Sites_attributes["oc"].isnull() == False)]
@@ -74,7 +77,7 @@ print(len(Sites_attributes.pedon_key.unique()))
 
 ## Calculate SOC for each pedon 
 
-# a function to sum the amount of C in all of the horizons. This function calculates the amount of OC in each increment as depth of increment*OC
+# pedon_sum: a function to sum the amount of C in all of the horizons. This function calculates the amount of OC in each increment as depth of increment*OC
 def pedon_sum(pedon):
     carbon_sum = 0
     for i in range(len(pedon)):
@@ -101,12 +104,13 @@ def create_pedons(df):
         #print(i)
         site_subset = df[(df["pedon_key"]==i)]
         pedon = new_pedon(site_subset)
-        new_df = new_df.append(pedon.loc[0])
+        new_df = pd.concat([new_df, pedon])
+
     return new_df
 
 Sites_CSummed = create_pedons(Sites_attributes)
-print(Sites_CSummed.head())
-# Reset the index so that it isn't all 0s 
+#print(Sites_CSummed.head())
+### Reset the index so that it isn't all 0s 
 Sites_CSummed = Sites_CSummed.reset_index()
 
 
@@ -118,7 +122,7 @@ base = MN.plot(color='white', edgecolor='black', figsize=(11, 11))
 
 Sites_CSummed_gdf = gpd.GeoDataFrame(Sites_CSummed, geometry='geometry')
 
-#Match crs to our basemap
+### Match crs to our basemap
 Sites_CSummed_gdf = Sites_CSummed_gdf.set_crs("EPSG:4269")
 Sites_CSummed_gdf = Sites_CSummed_gdf.to_crs(MN.crs)
 
@@ -139,7 +143,7 @@ def get_year(filenames):
     return Years
 
 
-# get list of all landcover file directoryies in a dataframe with their year
+### get list of all landcover file directoryies in a dataframe with their year
 files = os.listdir("./MVP1_data/HistoricLandcover/")
 Years = get_year(files)
 d = {'file':files, 'Year':Years}
@@ -167,11 +171,54 @@ def sample_landcover(year_sub,year,LandCover):
     year_sub['landcover'] = [x for x in landcover.sample(coord_list)]
     return year_sub
 
-# make sure crs's line up
+### make sure crs's line up
+YearTest = "./MVP1_data/HistoricLandcover/CONUS_Historical_y1992.tif"
+Year = rasterio.open(YearTest)
 Sites_CSummed_gdf = Sites_CSummed_gdf.to_crs(Year.crs.data)
 
-# run my functions
+## run my functions
 Sites_landcover = get_landcover(Sites_CSummed_gdf,LandCover)
-print(Sites_landcover.head(), "Sites_landcover head")
+print("Sites_landcover head", Sites_landcover.head())
 
 ## Build Predictive model
+
+### define the columns we want as dependent (sum of total SOC) and independent (landcover and year)
+x = Sites_landcover[['landcover']]
+y = Sites_landcover['carbon_sum']
+
+### split the dataframe for 70% training, 30% validation
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
+
+### Assess the model :) 
+
+### establish baseline error
+mean_test = y_test.mean()
+baseline_error = abs(mean_test - y_test)
+print('Baseline absolute error', round(np.mean(baseline_error),2), 'g/cm^2')
+
+# Fit model using Random Forest Regressor
+
+from sklearn.ensemble import RandomForestRegressor
+
+# create regressor object
+regr = RandomForestRegressor(n_estimators = 100, random_state = 0)
+
+#Fit the regressor with dataframes x_train and y_train
+regr.fit(X_train, y_train)  
+
+# Run model on the training dataset
+y_pred=regr.predict(X_test)
+
+# Evaluate how the model did using mean square error.  
+from sklearn.metrics import mean_squared_error
+
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+rmse
+
+# or we can look at the absolute value of the difference between predicted and "actual"
+errors = abs(y_pred - y_test)
+print('Mean Absolute Error:', round(np.mean(errors), 2), 'g/cm^2')
--- README.txt
+++ README.txt
@@ -2,7 +2,32 @@
 # Modelling Soil Carbon Content
 
 ## Description of project
+This is the same project that I completed for my MVP1. 
 
 ## Major code chunks
+I tried to follow markdown formatting as best as I could in this script. The major code chunks are: 
+
+##Importing Packages
+##Import soils data (NCSS gdb) and subsetting to area of interest (MN)
+Originally, I wanted to edit my code for this section and consider the whole US as an area of interest. However, things really broke down when I tried to do this. 
+##Wrangling NCSS gdb into one dataframe
+- this is followed by reading in all of the data layers that I need from the dataframe (NCSS, carbon_extractions, bulk_density) 
+Calculating SOC for each pedon 
+- define three functions to do so 
+Map and inspect calculated SOC
+- I output a .png graph so that I can look at it here
+Extract landvoer information from HistoricLandcover modeled layers and join to soils data
+- 3 functions defined in this section 
+Build Predictive model 
+- includes testing and assessment of mean absolute error
 
 ## Why this represents a MVP
+Originally, I wanted to expand upon the actual code of my MVP1 and move the workflow into a batch submission form for MVP2. At first this seemed 
+entirely possible as moving the code in initially went well. However, I soon found that it was very difficult for me to expand and improve the workflow in a batch
+submission form - I am much more attacheched to jupyter notebooks than I thought. I initially broke some things (badly) and spent a lot of time 
+trying to get back to working code. From this process, I learned that I will probably always try to edit long workflows on a subset of data in a 
+Jupyter notebook environment, and move to batch submission only when I feel pretty confident that the workflow is working well. I also was able to 
+learn how to push to github from the MSI environment, which was a bit more tricky than from my local environment. I estimate that I spent around 10 
+hours on this project. 
+My friend Dustin Michels helped me through some github issues. 
+ 
